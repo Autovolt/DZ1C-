ff6aa0d87c4c77e9c2d19b0fe9c969880de50ef5
Василий <bryant24@mail.ru>
2022-10-15 15:01:37 +0500

    решил задачу 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число: ");
+int input = int.Parse(Console.ReadLine())!;
+
+if(input >= 2)
+{
+int temp = 1;
+ while(temp <= input)
+ {
+    if(temp % 2 == 0)
+    {
+        Console.Write(temp + " ");
+    }
+    temp++;
+ }
+}
+else
+{
+    Console.WriteLine("Число должно быть положительным от 2 и более ");
+    Console.WriteLine("Введите число: ");
+    int input2 = int.Parse(Console.ReadLine())!;
+    if (input2 >= 2)
+    {
+        int temp2 = 1;
+        while (temp2 <= input2)
+        {
+            if (temp2 % 2 == 0)
+            {
+                Console.Write(temp2 + " ");
+            }
+            temp2++;
+
+        }
+
+    }
+}
