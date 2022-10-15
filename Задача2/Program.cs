154037550f2cc8ea5121d61d6da88c46d8351f44
Василий <bryant24@mail.ru>
2022-10-15 12:59:55 +0500

    Сделал 2 задачу

---
+++ Задача2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine())!;
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine())!;
+
+if (a > b)
+{
+    Console.Write("max ");
+    Console.Write(a);
+
+}
+else
+{
+    Console.Write("max ");
+    Console.Write(b);
+}
