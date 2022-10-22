8f178256b2690e640589598fd1ef322249654dc2
Василий <bryant24@mail.ru>
2022-10-22 21:09:48 +0500

    Задача 10 решена

---
+++ Задача10/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int SecondDigit(int a){
+    int temp = a % 100;
+    int temp2 = temp / 10;
+    return temp2;
+}
+
+Console.WriteLine("Введите любое трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int result = SecondDigit(a);
+Console.WriteLine(result);
+
+
+//int temp = a % 100;
+//int temp2 = temp / 10;
+//Console.WriteLine(temp2);
+
+
