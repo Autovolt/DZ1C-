ca2ab9ea2a31c77e2beb60bf87d08d9a5ac77110
Василий <bryant24@mail.ru>
2022-11-10 21:56:02 +0500

    DZLesson4

---
+++ DZLesson4/Задача27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = (int)Math.Log10(a) + 1;
+void SumNumbers(int b)
+{
+    int sum = 0;
+    for (int i = 1; i <= b; i++)
+    {
+        sum = sum + a%10;
+        a = a/10;
+       
+    }
+    Console.WriteLine($"Сумма всех цифр в числе A равна {sum}");
+}
+SumNumbers(b);
---
+++ DZLesson4/Задача29/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+//452 -> 3
+//82 -> 2
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int lenght = 8;
+    int index = 0;
+    while (index<lenght)
+{
+    Console.Write($"Введите число {index+1} массива. ");
+    collection[index] = Convert.ToInt32 (Console.ReadLine());;
+    index++;
+}
+}
+void PrintArray(int[] array)
+{
+    int count = 8;
+    for ( int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
