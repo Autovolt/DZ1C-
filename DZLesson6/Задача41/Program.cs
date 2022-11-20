6df5ed516ebd53ee969efaeea50a930176e08031
Василий <bryant24@mail.ru>
2022-11-20 19:48:59 +0500

    DZ Lesson 6

---
+++ DZLesson6/Задача41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+void CreateFillArray()
+{
+    Console.Write("Введите 5 чисел (через пробел): ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Чисел больше нуля: {count}");
+}
+CreateFillArray();
