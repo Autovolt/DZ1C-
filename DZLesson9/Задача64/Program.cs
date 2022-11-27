b597bbcaa5779e892ed7a4ecdd76e0fb51a7eac1
Василий <bryant24@mail.ru>
2022-11-27 22:40:45 +0500

    DZLesson9

---
+++ DZLesson9/Задача64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не натуральное число");
+}
+else Console.WriteLine(Recurtion(n, m));
+
+int Recurtion(int n, int m)
+{
+
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{Recurtion(n, m + 1)}, ");
+    return m;
+}
---
+++ DZLesson9/Задача66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Sum(int n)
+{
+    if (n==m) return 0;
+    return  n + Sum(n - 1); 
+}
+
+Console.WriteLine(m + Sum(n));
